52cf1c577908dcc200529e8b29fae0b7135b8545
Evan Earle <evan.earle@gmail.com>
2024-02-23 10:36:11 -0400

    set up try/catch block, launched a browser and tested it

--- server/scrape.js
+++ server/scrape.js
@@ -1,3 +1,24 @@
+import puppeteer from "puppeteer";
+
 export const scrape = async (res) => {
-  res.send("test");
+  // Launch a browser
+  const browser = await puppeteer.launch();
+  try {
+    // Initialize a page
+    const page = await browser.newPage();
+
+    // Navigate to a page
+    await page.goto("https://forums.redflagdeals.com/hot-deals-f9/");
+
+    // Testing
+    const title = await page.evaluate(() => document.title);
+    console.log(title);
+    res.send(title);
+  } catch (err) {
+    console.log(err);
+    res.send("Error");
+  } finally {
+    // Close the browser
+    await browser.close();
+  }
 };
